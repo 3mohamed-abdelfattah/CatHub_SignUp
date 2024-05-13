4b667f286638803f38296938a58304bf6aa626c0
MohamedMohamed <m01126165016@icloud.com>
2024-05-13 16:02:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/animations/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.animations
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
